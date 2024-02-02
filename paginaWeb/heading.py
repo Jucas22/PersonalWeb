53755b9709a66ec301ad6a00a6477fb4d5e0b689
Jucas22 <Juancastellobeltran09@gmail.com>
2024-02-02 13:30:27 +0100

    Puesta a punto para windows
    Puesta a punto para Windows

--- paginaWeb/heading.py
+++ paginaWeb/heading.py
@@ -1,34 +1,29 @@
 import flet as ft
 
+
 def heading(page: ft.page):
 
     heading_name = ft.Text(
-        "Juan Castelló",
-        size=70,
-        weight=ft.FontWeight.W_700,
-        selectable=True
+        "Juan Castelló", size=70, weight=ft.FontWeight.W_700, selectable=True
     )
     hedaing_descr = ft.Text(
         "Estudiante de Ingeniería informática en la Universidad Politécnica de Valéncia",
-        selectable=True
+        selectable=True,
     )
 
     img = ft.Image(
-        src=f"./profile.png",
+        src=f"./paginaWeb/profile.png",
         width=200,
         height=200,
         fit=ft.ImageFit.CONTAIN,
-        border_radius = ft.border_radius.all(99999)
+        border_radius=ft.border_radius.all(99999),
     )
 
-    heading_text = ft.Column(
-        [heading_name, hedaing_descr]
-    )
+    heading_text = ft.Column([heading_name, hedaing_descr])
     heading_imgText = ft.Row(
         [img, heading_text],
-        alignment = ft.MainAxisAlignment.CENTER,
+        alignment=ft.MainAxisAlignment.CENTER,
     )
-    
 
     heading = ft.Container(
         heading_imgText,
@@ -38,8 +33,8 @@ def heading(page: ft.page):
         #     colors=[ft.colors.BLUE, ft.colors.GREY]
         # ),
         height=250,
-        border = ft.border.all(5, ft.colors.BLACK)
+        border=ft.border.all(5, ft.colors.BLACK),
     )
-    
+
     page.add(heading)
     page.update()
